7acfda4f1dafb2675d45649c1348dca14cc2b43c
Koppala Naga Harsha <code.20135@gmail.com>
2026-02-12 17:07:55 +0530

    Add student greeting function in Basics.py

---
+++ Basics.py
@@ -0,0 +1,9 @@
+# Programming Practice - Narayana Junior College
+
+def student_greet():
+    college = "Narayana Junior College"
+    print("Welcome to my Python learning journey!")
+    print(f"Current Institution: {college}")
+
+if __name__ == "__main__":
+    student_greet()
